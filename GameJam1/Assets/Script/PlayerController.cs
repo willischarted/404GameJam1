8d647f38216fce0257e73ddb607cddac1c38f9da
William <willischarted@gmail.com>
2018-09-15 16:44:42 -0400

    Added a buncha stuff

---
+++ GameJam1/Assets/Script/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float scaleUpValue;
+	public float scaleDownValue;
+
+	public float speed;
+
+	public bool isMoving;
+
+	public bool isScalingDown;
+
+	private Rigidbody rigidBody;
+
+	public void Awake() {
+		isMoving = false;
+		isScalingDown = false;
+
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		isMoving = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		if (moveHorizontal!= 0 || moveVertical != 0)
+			isMoving = true;
+		else
+			isMoving = false;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidBody.AddForce (movement * speed);
+	}
+
+	public void ScaleUp(){
+		if (isMoving && !isScalingDown){
+			Debug.Log("scaling up");
+			transform.localScale = new Vector3(transform.localScale.x +scaleUpValue, 
+											transform.localScale.y +scaleUpValue,
+											transform.localScale.z +scaleUpValue);
+
+			//rigidBody.mass += 0.1f;	
+		}
+
+	}
+
+	public void ScaleDown(){
+		
+		Debug.Log("scaling down");
+		transform.localScale = new Vector3(transform.localScale.x -scaleDownValue, 
+										transform.localScale.y -scaleDownValue,
+										transform.localScale.z -scaleDownValue);
+		
+		//rigidBody.mass -=1;	
+
+	}
+
+	public bool getIsMoving() {
+		return isMoving;
+	}
+}

e01da0a760d828fc88e3a2c064026a0b5a676317
CalinArdelean <calin.a@hotmail.com>
2018-09-15 14:13:51 -0400

    Add files via upload

---
+++ P1Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Movement : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(Input.GetAxis("P1Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("P1Vertical") * Time.deltaTime * speed);
+
+    }
+}
---
+++ P2Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Movement : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Input.GetAxis("P2Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("P2Vertical") * Time.deltaTime * speed);
+
+    }
+}
